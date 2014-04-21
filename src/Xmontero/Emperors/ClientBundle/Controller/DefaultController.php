77ba7a378dde5864898cf0715a375a85d6c9c4f9
Xavi Montero <xmontero@dsitelecom.com>
2014-04-22 00:26:20 +0200

    Make the controller access the DB and the view to loop over rows in the DB.

--- src/Xmontero/Emperors/ClientBundle/Controller/DefaultController.php
+++ src/Xmontero/Emperors/ClientBundle/Controller/DefaultController.php
@@ -10,7 +10,42 @@ class DefaultController extends Controller
 {
 	public function rootAction( Request $request )
 	{
-		return $this->render( 'XmonteroEmperorsClientBundle:Pages:2014-04apr-home.html.twig', array( 'name' => $this->get( 'security.context' )->getToken() ) );
+		$connection = $this->getDoctrine()->getManager()->getConnection();
+		
+		$statement = $connection->prepare( 'SELECT * FROM emperorsFirstGame' );
+		$statement->execute();
+		$emperors = $statement->fetchAll();
+		
+		$statement = $connection->prepare( 'SELECT * FROM pawnsFirstGame' );
+		$statement->execute();
+		$pawns = $statement->fetchAll();
+		
+		$scope = array();
+		$scope[ 'name' ] = $this->get( 'security.context' )->getToken();
+		$scope[ 'emperors' ] = $emperors;
+		$scope[ 'pawns' ] = $pawns;
+		
+		return $this->render( 'XmonteroEmperorsClientBundle:Pages:2014-04apr-home.html.twig', $scope );
 		//return $this->render( 'XmonteroEmperorsClientBundle:Pages:home.html.twig', array( 'name' => $this->get( 'security.context' )->getToken() ) );
+	}
+	
+	public function dirtyAction( Request $request )
+	{
+		$connection = $this->getDoctrine()->getManager()->getConnection();
+		
+		$statement = $connection->prepare( 'SELECT * FROM emperorsFirstGame' );
+		$statement->execute();
+		$emperors = $statement->fetchAll();
+		
+		$statement = $connection->prepare( 'SELECT * FROM pawnsFirstGame' );
+		$statement->execute();
+		$pawns = $statement->fetchAll();
+		
+		$scope = array();
+		$scope[ 'name' ] = $this->get( 'security.context' )->getToken();
+		$scope[ 'emperors' ] = $emperors;
+		$scope[ 'pawns' ] = $pawns;
+		
+		return $this->render( 'XmonteroEmperorsClientBundle:Pages:2014-04apr-dirty.html.twig', $scope );
 	}
 }
