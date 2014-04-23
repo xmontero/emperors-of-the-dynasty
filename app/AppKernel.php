0e0c9c4ee3388e35339ddebaf77ca5086d3db6f0
Xavi Montero <xmontero@dsitelecom.com>
2014-04-24 01:03:46 +0200

    Add FOS User Bundle.

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,8 +17,11 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             
+            new FOS\UserBundle\FOSUserBundle(),
+            
             new Xmontero\Emperors\ServerBundle\XmonteroEmperorsServerBundle(),
             new Xmontero\Emperors\ClientBundle\XmonteroEmperorsClientBundle(),
+            new Xmontero\Emperors\UserBundle\XmonteroEmperorsUserBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
---
+++ src/Xmontero/Emperors/UserBundle/XmonteroEmperorsUserBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Xmontero\Emperors\UserBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class XmonteroEmperorsUserBundle extends Bundle
+{
+}
