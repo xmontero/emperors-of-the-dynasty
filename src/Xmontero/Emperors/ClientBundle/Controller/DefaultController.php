495d9894cd79bafda628c073435cd04a8359ff5b
Xavi Montero <xmontero@dsitelecom.com>
2014-04-25 08:50:22 +0200

    Add Layout. Split in several pages. Add board and matchings for the first game. Test to send an email.

--- src/Xmontero/Emperors/ClientBundle/Controller/DefaultController.php
+++ src/Xmontero/Emperors/ClientBundle/Controller/DefaultController.php
@@ -10,20 +10,8 @@ class DefaultController extends Controller
 {
 	public function rootAction( Request $request )
 	{
-		$connection = $this->getDoctrine()->getManager()->getConnection();
-		
-		$statement = $connection->prepare( 'SELECT * FROM emperorsFirstGame' );
-		$statement->execute();
-		$emperors = $statement->fetchAll();
-		
-		$statement = $connection->prepare( 'SELECT * FROM pawnsFirstGame' );
-		$statement->execute();
-		$pawns = $statement->fetchAll();
-		
 		$scope = array();
 		$scope[ 'name' ] = $this->get( 'security.context' )->getToken();
-		$scope[ 'emperors' ] = $emperors;
-		$scope[ 'pawns' ] = $pawns;
 		
 		return $this->render( 'XmonteroEmperorsClientBundle:Pages:2014-04apr-home.html.twig', $scope );
 		//return $this->render( 'XmonteroEmperorsClientBundle:Pages:home.html.twig', array( 'name' => $this->get( 'security.context' )->getToken() ) );
@@ -31,13 +19,29 @@ class DefaultController extends Controller
 	
 	public function dirtyAction( Request $request )
 	{
+		$scope = array();
+		$scope[ 'name' ] = $this->get( 'security.context' )->getToken();
+		
+		$message = \Swift_Message::newInstance()
+			->setSubject('Emperors are sending mails now!!!')
+			->setFrom('emperorsofthedynasty@gmail.com')
+			->setTo('xmontero@dsitelecom.com')
+			->setBody( $this->renderView( 'XmonteroEmperorsClientBundle:Pages:2014-04apr-dirty.html.twig', $scope ) );
+		
+		$this->get('mailer')->send($message);
+		
+		return $this->render( 'XmonteroEmperorsClientBundle:Pages:2014-04apr-dirty.html.twig', $scope );
+	}
+	
+	public function playersListAction( Request $request )
+	{
 		$connection = $this->getDoctrine()->getManager()->getConnection();
 		
-		$statement = $connection->prepare( 'SELECT * FROM emperorsFirstGame' );
+		$statement = $connection->prepare( 'SELECT * FROM emperorsFirstGame ORDER BY id' );
 		$statement->execute();
 		$emperors = $statement->fetchAll();
 		
-		$statement = $connection->prepare( 'SELECT * FROM pawnsFirstGame' );
+		$statement = $connection->prepare( 'SELECT * FROM pawnsFirstGame ORDER BY id' );
 		$statement->execute();
 		$pawns = $statement->fetchAll();
 		
@@ -46,6 +50,15 @@ class DefaultController extends Controller
 		$scope[ 'emperors' ] = $emperors;
 		$scope[ 'pawns' ] = $pawns;
 		
-		return $this->render( 'XmonteroEmperorsClientBundle:Pages:2014-04apr-dirty.html.twig', $scope );
+		return $this->render( 'XmonteroEmperorsClientBundle:Pages/FirstGame:players.html.twig', $scope );
 	}
+	
+	public function dumpPageAction( Request $request, $template )
+	{
+		$scope = array();
+		$scope[ 'name' ] = $this->get( 'security.context' )->getToken();
+		
+		return $this->render( $template, $scope );
+	}
+	
 }
