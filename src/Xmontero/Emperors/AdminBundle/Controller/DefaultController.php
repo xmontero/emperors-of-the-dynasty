eb3e6fd58c8e7fe19169fc703292db0e827d033d
Xavi Montero <xmontero@dsitelecom.com>
2014-05-11 04:05:26 +0200

    Create a dashboard fro the admin.

--- src/Xmontero/Emperors/AdminBundle/Controller/DefaultController.php
+++ src/Xmontero/Emperors/AdminBundle/Controller/DefaultController.php
@@ -10,15 +10,8 @@ class DefaultController extends Controller
 	public function rootAction( Request $request )
 	{
 		$scope = array();
-		$scope[ 'name' ] = $this->get( 'security.context' )->getToken();
 		
-		$message = \Swift_Message::newInstance()
-			->setSubject('Emperors are sending mails now!!!')
-			->setFrom('emperorsofthedynasty@gmail.com')
-			->setTo('xmontero@dsitelecom.com')
-			->setBody( $this->renderView( 'XmonteroEmperorsAdminBundle:Pages:dirty.html.twig', $scope ) );
-		
-		$this->get('mailer')->send($message);
+		$scope[ 'games' ] = $this->get( 'emperors.game.manager' )->getOpenGames( 10 );
 		
 		return $this->render( 'XmonteroEmperorsAdminBundle:Pages:dirty.html.twig', $scope );
 	}
