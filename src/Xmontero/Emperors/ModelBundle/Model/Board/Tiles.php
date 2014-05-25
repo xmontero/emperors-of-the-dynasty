dbea15f722466e69d231b3640cf81c10490fe6a8
Xavi Montero <xmontero@dsitelecom.com>
2014-05-25 17:05:16 +0200

    Refactor Board->oldLoad() into the Game class. Now Board does not depend on objectStorage and is generic.

--- src/Xmontero/Emperors/ModelBundle/Model/Board/Tiles.php
+++ src/Xmontero/Emperors/ModelBundle/Model/Board/Tiles.php
@@ -6,23 +6,4 @@ use Xmontero\Emperors\ModelBundle\Model\Base;
 
 class Tiles extends Base\Collection
 {
-	public function load( $document )
-	{
-	}
-	
-	public function saveToObject()
-	{
-		foreach( $this as $tile )
-		{
-			$tileSaveObject = $tile->saveObject();
-			$tiles[] = $tileSaveObject;
-		}
-		
-		return $object;
-	}
-	
-	public function saveToJson()
-	{
-		return json_encode( $this->saveToObject );
-	}
 }
--- src/Xmontero/Emperors/ModelBundle/Model/Game/Game.php
+++ src/Xmontero/Emperors/ModelBundle/Model/Game/Game.php
@@ -31,7 +31,94 @@ class Game
 	public function getBoard( $turn )
 	{
 		$board = $this->boardManager->loadBoardFromTemplate( 'uukhumaki' );
-		$board->oldLoad( $this->objectStorageManager, $turn, 14, 14 );
+		
+		switch( $turn )
+		{
+			case 0:
+			
+				$prefix = 'old';
+				break;
+			
+			case 1:
+			
+				$prefix = 'current';
+				break;
+			
+			case 2:
+			
+				$prefix = 'next';
+				break;
+		}
+		
+		$xName = $prefix . 'X';
+		$yName = $prefix . 'Y';
+		$servedDynastyName = $prefix . 'ServedDynasty';
+		$experienceName = $prefix . 'Experience';
+		
+		// Emperors
+		for( $i = 1; $i <= 8; $i++ )
+		{
+			$objectId = 'emperor-' . $i;
+			
+			$emperor = $this->objectStorageManager->getObjectById( $objectId );
+			
+			$x = $emperor[ $xName ];
+			$y = $emperor[ $yName ];
+			
+			$class = 'emperor-' . $i;
+			$content = 'E' . $i;
+			
+			$tile = $board->getTile( $x, $y );
+			$tile->setProperty( 'class', $class );
+			$tile->setProperty( 'text', $content );
+		}
+		
+		// Pawns
+		for( $player = 1; $player <= 8; $player++ )
+		{
+			for( $piece = 1; $piece <= 4; $piece++ )
+			{
+				$objectId = 'pawn-' . $player . '-' . $piece;
+				
+				$pawn = $this->objectStorageManager->getObjectById( $objectId );
+				
+				if( $pawn[ 'blob' ] != '**' )
+				{
+					$x = $pawn[ $xName ];
+					$y = $pawn[ $yName ];
+					
+					$specialClass = ( $pawn[ 'blob' ] == '*' ) ? 'special ' : '';
+					$class = $specialClass . 'pawn-' . $pawn[ $servedDynastyName ];
+					$content = 'P' . $player . $piece;
+					
+					$tile = $board->getTile( $x, $y );
+					$tile->setProperty( 'class', $class );
+					$tile->setProperty( 'text', $content );
+				}
+			}
+		}
+		
+		// Chests
+		for( $i = 1; $i <= 4; $i++ )
+		{
+			$objectId = 'chest-' . $i;
+			
+			$chest = $this->objectStorageManager->getObjectById( $objectId );
+			
+			if( $chest[ 'blob' ] != '**' )
+			{
+				$x = $chest[ $xName ];
+				$y = $chest[ $yName ];
+				
+				$class = 'chest-' . $i;
+				$content = 'C' . chr( $i + ord( 'A' ) - 1 );
+				
+				$tile = $board->getTile( $x, $y );
+				$tile->setProperty( 'class', $class );
+				$tile->setProperty( 'text', $content );
+			}
+		}
+		
 		return $board;
 	}
 	
